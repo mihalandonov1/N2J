945bf80d5a2ec32077467a4331e3d3c4b190a386
Mihal Andonov <mihal.andonov@gmail.com>
2023-06-06 01:48:29 +0300

    new sada dsadsad

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import MainPage1 from "./pages/page1/MainPage1";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage2 from "./pages/page2/MainPage2";
-
 import MainPage3 from "./pages/page3/MainPage3";
-import MainPage4 from "./pages/page4/MainPage4";
-import MainPage5 from "./pages/page5/MainPage5";
+
 import MainNav from "./MainNav";
 
 function App() {
@@ -14,8 +12,6 @@ function App() {
       <Route path="page1" element={<MainPage1 />} />
       <Route path="page2" element={<MainPage2 />} />
       <Route path="page3" element={<MainPage3 />} />
-      <Route path="page4" element={<MainPage4 />} />
-      <Route path="page5" element={<MainPage5 />} />
     </Routes>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,10 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import MainPage3 from "./pages/page3/MainPage3";
 import MainPage1 from "./pages/page1/MainPage1";
+import MainPage2 from "./pages/page2/MainPage2";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <MainPage2 />
   </BrowserRouter>
 );
