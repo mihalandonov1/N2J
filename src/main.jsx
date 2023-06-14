08996f448d32544f76f176a19f02ae2482707201
Mihal Andonov <mihal.andonov@gmail.com>
2023-06-14 06:49:21 +0300

    sda

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import MainPage3 from "./pages/page3/MainPage3";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <MainPage3 />
   </BrowserRouter>
 );
